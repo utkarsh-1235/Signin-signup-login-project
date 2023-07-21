443e177b9820e49eacd126599d5cd37594820e03
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-07-21 09:00:31 +0530

    adding cors

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,23 @@ const app = express();
 
 const authRouter = require('./Routes/authRoute')
 const connectTodb = require('./Config/db')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
 
 // Database connected
  connectTodb()
+
 app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+   origin: [process.env.CLIENT_URL],
+   credentials: true
+}))
+
 app.use("/api/auth/",authRouter)
+
+
 app.use("/",(req,res)=>{
    res.status(200).json({
     data:"JWT auth server"
