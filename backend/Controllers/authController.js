612a1528c4a3cbb95ad2dbe24d849476b8c0b25f
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-07-21 08:56:04 +0530

    adding features of forgot and reset password

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,6 +1,7 @@
 const userModel = require('../Models/userSchema')
 const emailValidator = require('email-validator')
-
+const crypto = require('crypto')
+const bcrypt = require('bcrypt')
 
 
 // sign up controller
@@ -8,7 +9,7 @@ const emailValidator = require('email-validator')
  const signup = async(req,res,next)=>{
     const{name,email,password,confirmPassword} = req.body
     console.log(name,email,password,confirmPassword)
-
+    //res.send({msg:"nothing"})
     // If anyone from name, email, password, confirmPassword not provide throw error
 
     if(!name || !email || !password || !confirmPassword){
@@ -33,28 +34,30 @@ const emailValidator = require('email-validator')
     if(password !== confirmPassword){
         return res.status(400).json({
             success:false,
-            message:"You enter wrong password"
+            message:"password and confirm password not match"
         })
     }
     try{
         const userInfo = userModel(req.body);
+       
+        // userSchema "pre" middleware functions for "save" will hash the password using bcrypt
+    // before saving the data into the database
 
         const result = await userInfo.save()
     
        return res.status(200).json({
-            success:true,
-            data:{result}
+            success : true,
+            data : {result}
         })
     }
     catch(err){
-        if(e.code === 11000){
+        if(err.code === 11000){
             return res.status(400).json({
                 success:false,
-                message:'account already exists with provided email'
+                message:`account already exists with provided email ${email}`
             })
         }
         return res.status(400).json({
-            success:false,
             message:e.message
         })
     }
@@ -81,10 +84,10 @@ const emailValidator = require('email-validator')
          email
         })
         .select("+password")
- 
+         
      //  If user not exist and password not matches to actual password then show error
- 
-        if(!user || user.password === password){
+   
+        if(!user || await bcrypt.compare(password, user.password )){
               return res.status(400).json({
                  success:false,
                  message:"invalid credentials"
@@ -114,7 +117,103 @@ const emailValidator = require('email-validator')
     }
 
 
+    const forgotPassword = async(req,res,next)=>{
+        const email = req.body.email
 
+
+        // return response with error message If email is undefined
+        if(!email){
+            return res.status(400).json({
+                success: false,
+                message: "Email is required"
+            })
+        }
+
+        try{
+            const user = await userModel.findOne({
+                email
+            })
+
+            // return response with error messgae user not found
+            if(!user){
+                return res.status(400).json({
+                    success: false,
+                    message: "user not found"
+                })
+            }
+
+            // Generate the token with userSchema method getForgotpasswordToken().
+
+            const forgotPasswordToken = user.getForgotPasswordToken()
+            console.log(forgotPasswordToken)
+            await user.save();
+
+            return res.status(200).json({
+                success: true,
+                token: forgotPasswordToken
+            })
+            
+        }
+        catch(err){
+            return res.status(400).json({
+                success: false,
+                message: err.message
+            })
+        }
+    }
+
+
+    //Reset Password
+    const ResetPassword = async(req,res,next)=>{
+          const {token} = req.params;
+          const {password, confirmPassword } = req.body
+
+          // return error message if password or confirmPassword is missing
+          if(!password || !confirmPassword){
+            return res.status(400).json({
+                success: false,
+                message: "password and confirmPassword required"
+            })
+          }
+
+          //return error if password and confirmPassword are not same
+          if(password !== confirmPassword){
+            return res.status(400).json({
+                success: false,
+                message: "password and confirmPassword are not match"
+            })
+          }
+          const hashToken = crypto.createHash("sha256").update(token).digest("hex");
+
+          try{
+            const user = await userModel.findOne({
+                forgotPasswordToken: hashToken,
+                forgotPasswordExpiryDate: {
+                    $gt: new Date()  // forgot passwordExpiryDate() less the current Date
+                }
+            })
+
+            //return the message if user not found
+            if(!user){
+                return res.status(400).json({
+                    success: false,
+                    message: "Invalid Token or token is expired"
+                })
+            }
+            user.password = password
+            await user.save()
+            return res.status(200).json({
+                success: true,
+                message: "successfully reset the password"
+            })
+          }
+          catch(err){
+            return res.status(400).json({
+                success: false,
+                message: err.message
+            })
+          }
+    }
 
    // login user
     const getUser = async(req,res)=>{
@@ -133,8 +232,37 @@ const emailValidator = require('email-validator')
             })
         }
     }
+
+
+    // logout
+    const logout = async(req,res)=>{
+          try{
+            const cookieOption = {
+                expires : new Date(),
+                httpOnly : true
+            }
+               res.cookie("token", null, cookieOption)
+               res.status(200).json({
+                success : true,
+                message : "Loged Out"
+               })
+          }
+          catch(err){
+               res.status(400).json({
+                  success : false,
+                  message : err.message
+          })
+          }
+    }
+
+
+
  module.exports = {
     signup, 
     signin, 
-    getUser
+    forgotPassword,
+    getUser,
+    ResetPassword,
+    logout,
+   
  }
--- backend/Routes/authRoute.js
+++ backend/Routes/authRoute.js
@@ -1,14 +1,24 @@
 const express = require('express')
-const jwtAuth = require('../Middleware/jwtAuth')
+const authRoute = express.Router()
+
+
+const jwtAuth = require('../Middleware/jwtAuth.js')
 const {signup,
        signin, 
+       forgotPassword,
+       ResetPassword,
        getUser, 
-       logout} = require('../Controllers/authController')
-const authRoute = express.Router()
+       logout,
+} = require('../Controllers/authController.js')
+
 
 authRoute.post("/signup",signup)
 authRoute.post("/signin",signin)
+authRoute.post("/forgotPassword",forgotPassword)
+authRoute.post("/ResetPassword/:token",ResetPassword)
+
+
 authRoute.get("/user",jwtAuth,getUser)
-//authRoute.get("/logout",jwtAuth,logout)
+authRoute.get("/logout",jwtAuth,logout)
 
 module.exports = authRoute
