a1c8587c2e2d7086a47e1847078ccb99cb7954e4
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-07-26 03:51:48 +0530

    getForgotPasswordTken adding

--- backend/Models/userSchema.js
+++ backend/Models/userSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const JWT = require('jsonwebtoken')
 require('dotenv').config()
+const bcrypt = require('bcrypt')
 //Defining userSchema on which database store the data
 const userSchema = mongoose.Schema({
    name:{
@@ -33,6 +34,14 @@ const userSchema = mongoose.Schema({
     timestamps:true
 })
 
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        return next()
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+    return next()
+})
 userSchema.methods = {
     jwtToken(){
         return JWT.sign({
@@ -40,7 +49,25 @@ userSchema.methods = {
             process.env.SECRET,
             { expiresIn: '24h'}
         )
-    }
+    },
+
+
+
+//userSchema method for generating and return forgotPassword token
+getForgotPasswordToken() {
+    const forgotToken = crypto.randomBytes(20).toString('hex')
+    //step 1 - save to DB
+    this.forgotPasswordToken = crypto
+      .createHash('sha256')
+      .update(forgotToken)
+      .digest('hex');
+
+    /// forgot password expiry date
+    this.forgotPasswordExpiryDate = Date.now() + 20 * 60 * 1000 // 20min
+
+    //step 2 - return values to user
+    return forgotToken;
+  }
 }
 // console.log(process.env.SECRET)
 const userModel = mongoose.model('user',userSchema)
